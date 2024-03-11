2e91c17e4d7ce9c61f4bc023a5c294bf1edc843c
Noman Mia <nomanislamnishad@gmail.com>
2024-03-11 19:20:28 +0600

    added visited button and event handle

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,7 +1,15 @@
 
+import { useState } from 'react';
 import './country.css';
 const Country = ({country}) => {
     const {name, flags,population, area,cca3} = country;
+
+    const [visited, setVisited] = useState(false)
+
+    const handleVisited = () => {
+        setVisited(!visited);
+    }
+
     return (
         <div className='country'>
             <h3>Country : {name?.common} </h3>
@@ -9,6 +17,10 @@ const Country = ({country}) => {
             <p>Population : {population}</p>
             <p>Area : {area}</p>
             <p><small>Cca3 : {cca3}</small></p>
+            <button onClick={handleVisited}>{visited? "Visited" : "going"}</button>
+            {
+                visited? 'already visited' : 'i will visit'
+            }
         </div>
     );
 };
